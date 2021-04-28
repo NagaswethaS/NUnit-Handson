0d0ef6c9231de7e736dd305faacd2720cf2e4eed
NagaswethaS <nagaswethamec99@gmail.com>
2021-04-28 11:12:31 +0530

    first commit

---
+++ NUnitMoqHandson_8/CustomerCommLib/CustomerComm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerCommLib
+{
+    public class CustomerComm
+    {
+        IMailSender _mailSender;
+       
+        public CustomerComm(IMailSender mailSender)
+        {
+            this._mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            _mailSender.SendMail("abcd@gmal.com", "Hallo User");
+            return true;
+        }
+    }
+}
